01d1521cad19770ccccc44d332668a0fe734fe3b
Çağatay Çelimli <c.celimli@live.co.uk>
2023-06-30 00:36:14 +0300

    www.patika.dev

--- arrayFrekans/src/Main.java
+++ arrayFrekans/src/Main.java
@@ -1,25 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 1, 2, 3, 2, 4, 5, 5, 4, 6, 7, 7, 6, 8, 9, 8, 10};
-        boolean[] visited = new boolean[numbers.length];
-        Integer temp;
-        Integer count=0;
-        temp=numbers[0];
-        for (int i=1; i<numbers.length;i++){
-            if (visited[i]) {
-                continue;
-            }
-            for(int j=0; j<numbers.length; j++){
-                if (temp==numbers[j]){
-                    visited[j] = true;
-                    count++;
+
+        System.out.println("Dizi: " + Arrays.toString(numbers));
+        Arrays.sort(numbers);
+
+        int counter = 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 0) {
+                for (int j = 0; j < numbers.length; j++) {
+                    if ((i != j) && (numbers[i] == numbers[j])) {
+                        counter++;
+                    }
                 }
-                temp=numbers[i];
+                System.out.println(numbers[i] + " sayısı " + counter + " kere tekrar edildi.");
+            } else if (numbers[i] != numbers[i-1]) {
+                for (int j = 0; j < numbers.length; j++) {
+                    if ((i != j) && (numbers[i] == numbers[j])) {
+                        counter++;
+                    }
+                }
+                System.out.println(numbers[i] + " sayısı " + counter + " kere tekrar edildi.");
             }
-            temp=numbers[i];
-            System.out.println(temp+" sayısı "+count+" kere tekrar edildi.");
-            count=0;
+            counter = 1;
         }
+
 
     }
 }
